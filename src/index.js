60c35416ecedfdb67dfc326e4eac1bb3df9b8b82
Ciara Jones <129469388+Goalden1@users.noreply.github.com>
2024-01-21 17:06:32 -0500

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+function updateForecastData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  
+}
+
+
+
+function searchCity(city) {
+let apiKey = "b6c40f3b5f0t35ob0a85fa4f8c34ae03";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateForecastData);
+}
+
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML =searchInput.value;
+  
+ 
+ 
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Cincinnati")

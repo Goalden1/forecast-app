a6892ff160ec938945e2bdc839c740ad8cfd44be
Ciara Jones <129469388+Goalden1@users.noreply.github.com>
2024-01-22 00:43:48 -0500

    Elements repositioned, icon id and Url added,

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ function updateForecastData(response) {
   let timeElement=document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
+  let iconElement = document.querySelector("#icon"
+  );
+
+   iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="forecast-app-icon"
+              />`;
+
 
 
   cityElement.innerHTML = response.data.city;
